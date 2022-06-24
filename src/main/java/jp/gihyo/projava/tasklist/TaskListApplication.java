a6b034cbcd1a34f5798ec74706f68ef1f2b66312
mediakurita <t.kurita@mediajoho.com>
2022-06-24 11:09:46 +0900

    Init comit

--- src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskListApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(TaskListApplication.class, args);
 	}
 
